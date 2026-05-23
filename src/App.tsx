c7cd9c5621b81cb9c851946028f6c8e99e99438c
tresbits010 <tresbits010@gmail.com>
2026-05-22 21:58:48 -0300

    se

--- src/App.tsx
+++ src/App.tsx
@@ -15,13 +15,11 @@ import {
   ChevronDown,
   HelpCircle,
   Flame,
-  Smartphone,
   TrendingUp,
   Activity,
   Bot,
   Star,
   Clock,
-  Target,
   Wrench,
   Send
 } from "lucide-react";
@@ -725,24 +723,4 @@ useEffect(() => {
       </footer>
     </main>
   );
-}
-
-function User(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-      <circle cx="12" cy="7" r="4" />
-    </svg>
-  )
 }
